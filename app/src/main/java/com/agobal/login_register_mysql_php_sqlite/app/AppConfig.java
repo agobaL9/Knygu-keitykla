d97d9c4f392926dc841d5d9e5684f4e70ed8d26c
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-05-06 17:52:37 +0300

    First commit

---
+++ app/src/main/java/com/agobal/login_register_mysql_php_sqlite/app/AppConfig.java
@@ -0,0 +1,10 @@
+package com.agobal.login_register_mysql_php_sqlite.app;
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.150/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.150/android_login_api/register.php";
+}
